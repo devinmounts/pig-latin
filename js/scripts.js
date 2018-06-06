df06fc60152b34388729a8526d54e01d70b404f2
Cynthia Smith and Devin Mounts <cynthia@>
2018-06-06 13:49:29 -0700

    create if statement to prove ifinput is not a number.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,38 @@
-var alphabetArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v", "w","x","y","z"];
-var word = $("input#word").val();
-var splitWord = word.split("");
-console.log(splitWord)
 
 
-  // if (alphabetArray.includes(splitWord)) {
-  //   return true;
-  // });
+
+
+function pigLatin(string) {
+  var alphabetArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z"];
+
+  var splitWord = [];
+
+  splitWord = string.split("");
+  if (isNaN(splitWord)){
+    return "not a number";
+  }
+
+
+  // for(i=0; i<=splitWord.length; i++) {
+  //   if(alphabetArray[i] === splitWord[0]) {
+  //     alert("it's a letter");
+  //     return;
+  //   } else {
+  //     alert("not a letter");
+  //     return;
+  //   }
+  // }
+
+
+}
 
 $(function(){
   $("form#pig-latin").submit(function(e){
     e.preventDefault();
-    var result = translated(word);
+    var word = $("input#word").val();
+    pigLatin(word);
+
+    var result = pigLatin(word);
    $("#result").text(result);
  });
 });
