0de06198266edaff8f70b6d83380b1297071508e
Cynthia Smith and Devin Mounts <devin@>
2018-06-06 15:35:34 -0700

    add for loop that determines if first letter of string is vowel.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,21 @@
-
-
-
-
 function pigLatin(string) {
-  var alphabetArray = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z"];
+  var vowels = ["a","e","i","o","u"];
 
   var splitWord = [];
 
   splitWord = string.split("");
-  if (isNaN(splitWord)){
-    return "not a number";
-  }
+  // if (isNaN(splitWord)){
+    // return "not a number";
+  // } else if {
+  //
+  // }
 
+  for(i = 0; i < vowels.length; i++) {
+    if(splitWord[0] == vowels[i]) {
+      alert("this is a vowel");
+    }
+  }
+}
 
   // for(i=0; i<=splitWord.length; i++) {
   //   if(alphabetArray[i] === splitWord[0]) {
@@ -24,13 +28,12 @@ function pigLatin(string) {
   // }
 
 
-}
+// }
 
 $(function(){
   $("form#pig-latin").submit(function(e){
     e.preventDefault();
     var word = $("input#word").val();
-    pigLatin(word);
 
     var result = pigLatin(word);
    $("#result").text(result);
